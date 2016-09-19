8884a728ffd4a9b4177e48adcb18ecedd68eae95
Sebastian Lavie <sebastian.lavie@gmail.com>
2016-09-19 11:33:32 -0300

    # Rename liquibase changelog files and folders # Add tables: countries, states, cities, franchises, franchiseds, franchise_branches

--- com.bistro.sagg.database/src/main/resources/liquibase/migrate/0.0.3.SNAPSHOT/migrate-db-model-changelog-1.sql
+++ com.bistro.sagg.database/src/main/resources/liquibase/migrate/0.0.3.SNAPSHOT/migrate-db-model-changelog-1.sql
@@ -2,7 +2,7 @@
 --  Update Database Script
 --  *********************************************************************
 --  Change Log: src/main/resources/liquibase/master-changelog.xml
---  Ran at: 17-09-16 16:32
+--  Ran at: 19-09-16 10:28
 --  Against: root@localhost@jdbc:mysql://localhost:3306/sagg?createDatabaseIfNotExist=true
 --  Liquibase version: 3.5.1
 --  *********************************************************************
@@ -16,15 +16,15 @@ DELETE FROM DATABASECHANGELOGLOCK;
 INSERT INTO DATABASECHANGELOGLOCK (ID, LOCKED) VALUES (1, 0);
 
 --  Lock Database
-UPDATE DATABASECHANGELOGLOCK SET LOCKED = 1, LOCKEDBY = 'SEBASTIAN-ENVI (192.168.1.115)', LOCKGRANTED = '2016-09-17 16:32:08.354' WHERE ID = 1 AND LOCKED = 0;
+UPDATE DATABASECHANGELOGLOCK SET LOCKED = 1, LOCKEDBY = 'SEBASTIAN-ENVI (192.168.1.115)', LOCKGRANTED = '2016-09-19 10:28:57.414' WHERE ID = 1 AND LOCKED = 0;
 
 --  Create Database Change Log Table
 CREATE TABLE DATABASECHANGELOG (ID VARCHAR(255) NOT NULL, AUTHOR VARCHAR(255) NOT NULL, FILENAME VARCHAR(255) NOT NULL, DATEEXECUTED datetime NOT NULL, ORDEREXECUTED INT NOT NULL, EXECTYPE VARCHAR(10) NOT NULL, MD5SUM VARCHAR(35) NULL, DESCRIPTION VARCHAR(255) NULL, COMMENTS VARCHAR(255) NULL, TAG VARCHAR(255) NULL, LIQUIBASE VARCHAR(20) NULL, CONTEXTS VARCHAR(255) NULL, LABELS VARCHAR(255) NULL, DEPLOYMENT_ID VARCHAR(10) NULL);
 
---  Changeset src/main/resources/liquibase/1.0.0.qualifier/db-model-changelog-1.xml::1::sebastian.lavie
-CREATE TABLE employees (id BIGINT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NULL, person_id VARCHAR(12) NOT NULL, email VARCHAR(50) NULL, phone VARCHAR(15) NULL, cellphone VARCHAR(15) NULL, CONSTRAINT PK_EMPLOYEES PRIMARY KEY (id), UNIQUE (id));
+--  Changeset src/main/resources/liquibase/0.0.3-SNAPSHOT/db-model-changelog-1.xml::1::sebastian.lavie
+CREATE TABLE employees (id BIGINT AUTO_INCREMENT NOT NULL, firstname VARCHAR(25) NULL, lastname VARCHAR(25) NULL, person_id VARCHAR(12) NOT NULL, email VARCHAR(50) NULL, phone VARCHAR(15) NULL, cellphone VARCHAR(15) NULL, CONSTRAINT PK_EMPLOYEES PRIMARY KEY (id), UNIQUE (id));
 
-INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('1', 'sebastian.lavie', 'src/main/resources/liquibase/1.0.0.qualifier/db-model-changelog-1.xml', NOW(), 1, '7:57300fb9792319ca17e34372c5070477', 'createTable tableName=employees', '', 'EXECUTED', NULL, NULL, '3.5.1', '4140729343');
+INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('1', 'sebastian.lavie', 'src/main/resources/liquibase/0.0.3-SNAPSHOT/db-model-changelog-1.xml', NOW(), 1, '7:9ebac9ed4580337498796f06528b4b80', 'createTable tableName=employees', '', 'EXECUTED', NULL, NULL, '3.5.1', '4291738472');
 
 --  Release Database Lock
 UPDATE DATABASECHANGELOGLOCK SET LOCKED = 0, LOCKEDBY = NULL, LOCKGRANTED = NULL WHERE ID = 1;
