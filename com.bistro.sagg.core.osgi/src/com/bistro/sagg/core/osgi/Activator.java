aed6996e60c3e11cbfee121ed208abb5f5679acb
Sebastian Lavie <sebastian.lavie@gmail.com>
2016-10-23 09:58:43 -0300

    Project configurations

--- com.bistro.sagg.core.osgi/src/com/bistro/sagg/core/osgi/Activator.java
+++ com.bistro.sagg.core.osgi/src/com/bistro/sagg/core/osgi/Activator.java
@@ -8,8 +8,10 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 import com.bistro.sagg.core.model.company.FranchiseBranch;
+import com.bistro.sagg.core.model.location.Country;
 import com.bistro.sagg.core.services.EmployeeServices;
 import com.bistro.sagg.core.services.FranchiseServices;
+import com.bistro.sagg.core.services.RefdataServices;
 import com.bistro.sagg.core.services.SaggServiceLocator;
 import com.bistro.sagg.core.session.SaggSession;
 import com.bistro.sagg.core.session.SaggSessionConstants;
@@ -53,8 +55,16 @@ public class Activator extends AbstractUIPlugin {
 				.lookup(FranchiseServices.class.getName());
 		EmployeeServices employeeService = (EmployeeServices) SaggServiceLocator.getInstance()
 				.lookup(EmployeeServices.class.getName());
+//		RefdataServices refdataService = (RefdataServices) SaggServiceLocator.getInstance()
+//				.lookup(RefdataServices.class.getName());
 		// TODO Get correct session objects
 		SaggSession.getCurrentSession().addSessionObject(SaggSessionConstants.CURRENT_FRANCHISE_BANCH, franchiseService.getById(1L));
+		
+		Country country = new Country();
+		country.setId(1L);
+		country.setName("Chile");
+		SaggSession.getCurrentSession().addSessionObject(SaggSessionConstants.CURRENT_FRANCHISE_BANCH_COUNTRY, country);
+
 		SaggSession.getCurrentSession().addSessionObject(SaggSessionConstants.ACTIVE_USER, employeeService.getById(1L));
 		SimpleDateFormat formatter = new SimpleDateFormat("EEEE d 'de' MMMM 'de' yyyy HH:mm", new Locale("es","CL"));
 		SaggSession.getCurrentSession().addSessionObject(SaggSessionConstants.DATE_FORMATTER, formatter);
